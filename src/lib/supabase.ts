823eceff1967cc97322f832d3bf926adfdb56428
Samprad <sampraaddas2503@gmail.com>
2025-07-26 15:44:22 +0530

    hardcoded supabase variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://bhrxieqrtfudpkupdtsx.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJocnhpZXFydGZ1ZHBrdXBkdHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MDc0MzYsImV4cCI6MjA2OTA4MzQzNn0.B_Nb7BcDqk9Vt-z4AwDcjDaTRkpCb3-NqGePSbhyCmU";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables");
